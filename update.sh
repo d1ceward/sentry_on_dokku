d673429db9d27b8d4b2b281018f3b67c48e4ee86
d1ceward <d1ceward@gmail.com>
2019-04-05 15:30:24 +0200

    Fixed script

--- update.sh
+++ update.sh
@@ -1,20 +1,19 @@
 # Get lastest release name
-RELEASE_SENTRY=$(curl --silent "https://github.com/getsentry/sentry/releases/latest" | sed 's#.*tag/\(.*\)\".*#\1#' | cut -f2 -d 'v')
-RELEASE_SENTRY_PLUGINS=$(curl --silent "https://github.com/getsentry/sentry-plugins/releases/latest" | sed 's#.*tag/\(.*\)\".*#\1#' | cut -f2 -d 'v')
+RELEASE=$(curl --silent "https://github.com/getsentry/sentry/releases/latest" | sed 's#.*tag/\(.*\)\".*#\1#')
 
 # Replace version line in requirements.txt with the new release
-sed -i "s#sentry==.*#sentry==${RELEASE_SENTRY}#" requirements.txt
-sed -i "s#sentry-plugins==.*#sentry-plugins==${RELEASE_SENTRY_PLUGINS}#" requirements.txt
+sed -i "s#sentry==.*#sentry==${RELEASE}#" requirements.txt
+sed -i "s#sentry-plugins==.*#sentry-plugins==${RELEASE}#" requirements.txt
 
 # Replace README link to grafana release
-SENTRY_BADGE="[![Sentry](https://img.shields.io/badge/Sentry-${RELEASE_SENTRY}-blue.svg)](https://github.com/getsentry/sentry/releases/tag/${RELEASE_SENTRY})"
+SENTRY_BADGE="[![Sentry](https://img.shields.io/badge/Sentry-${RELEASE}-blue.svg)](https://github.com/getsentry/sentry/releases/tag/${RELEASE})"
 sed -i "s#\[\!\[Sentry\].*#${SENTRY_BADGE}#" README.md
 
 # Push changes
-git add Dockerfile README.md
-git commit -m "Update to sentry version v${RELEASE_SENTRY}"
+git add requirements.txt README.md
+git commit -m "Update to sentry version v${RELEASE}"
 git push origin master
 
 # Create tag
-git tag "v${RELEASE_SENTRY}"
+git tag "v${RELEASE}"
 git push --tags
